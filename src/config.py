22012039290adaf3ee9d077400335f2d5f733914
asiokfd <asiokfd@LAPTOP-T2VJVASV.localdomain>
2021-05-17 16:58:54 +0200

    comit

--- src/config.py
+++ src/config.py
@@ -2,9 +2,9 @@ dic_categorias= {
     
     "restaurantes": ["restaurant"],
     "playas": "4bf58dd8d48988d1e2941735",
-    "ocio_nocturno": ["pub", "bar", "disco","Beer"],
+    "ocio_nocturno": ["pub","Cocktail Bar"],
     "hoteles": ["hotel"],
-    "ocio_deporte": ["sport", "gym", "Fitness"],
+    "ocio_deporte": ["gym", "Fitness"],
     "colegios" : ["school", "primary school", "elemental school"],
     "guarderias" : ["kindergarden", "preschool"],
     "universidades": ["college", "university"],
@@ -14,11 +14,11 @@ dic_categorias= {
     "hospitales" : ["hospital", "medical center"],
     "cines": ["cinema", "movie theatre"],
     "teatros": ["theatre"],
-    "museos": ["musseum"],
-    "salas_de_conciertos": ["rock club", "jazz club", "piano bar"],
+    "museos": ["museum"],
+    "salas_de_conciertos": ["rock club", "jazz club", "concert"],
     "piscina_para_Emilio": ["pool"],
-    "bibliotecas" : ["library"],
-    "centros_comerciales": ["mall" ]
+    "bibliotecas" : ["library", "book store"],
+    "centros_comerciales": ["mall", "shopping center"]
     
     
 }
