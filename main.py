58ed0e8024ad478ddaa4816522089bd2cd56921f
Alberto Pérez <asiokfd@gmail.com>
2021-05-19 08:53:46 +0200

    miercoles

--- main.py
+++ main.py
@@ -15,18 +15,18 @@ st.write("""
 """)
 
 
-col1,col2 = st.beta_columns([2,2])
+col1,col2 = st.beta_columns([2,3])
 imagen = Image.open("images/portada2.jpg")
 imagen2= Image.open("images/portada.jpg")
 col1.image(imagen)
-col2.bar_chart (dat.grafica_intro())
+col2.bar_chart (dat.grafica_intro2())
 
 
-st.write(""" Bienvenidos a CityAdvisor! Tu comparador de ciudades!
+st.write(""" Bienvenidos a CityAdvisor! Tu comparador urbano!
 
 """)
 
-col1,col2 = st.beta_columns([2,2])
+col1,col2 = st.beta_columns([3,2])
                                             
 col1.dataframe (dat.grafica_intro())
 col2.image (imagen2)
@@ -54,7 +54,7 @@ st.write ("usted ha elegido:", ubicacion)
 
 items = st.multiselect (
 
-    "Ahora añade tus búsquedas", dat.lista_items()
+    "Ahora añade tus búsquedas", dat.lista_sub()
 )
 if not items:
    st.warning('Por favor, seleccione una categoría')
@@ -68,34 +68,8 @@ else:
     folium_static (mapa, width=900, height=600)
 
 
-col1,col2 = st.beta_columns ([1,3])
-col1.dataframe (dat.grafica_post(ubicacion))
-col2.bar_chart (dat.grafica_post (ubicacion))
-
-"""
-uploaded_file = st.file_uploader("Sube un csv")
-
-if uploaded_file: 
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
-
-foto = st.file_uploader("Sube una foto")
-if foto:
-    imagen = Image.open(foto)
-    imagen.save("data/foto.png")
-    st.write ("tu foto se ha subido correctamente")
-
-
-st.write("Columnas")
-
-
-
-col1,col2 = st.beta_columns([4,2])
-
-col1.subheader("El Gráfico")
-
-
-col2.subheader("Los datos")
-col2.write ("aqui")
-
-"""
+col1,col2 = st.beta_columns ([2,2])
+col1.dataframe (dat.grafica_cat(ubicacion))
+col1.subheader ("Totales por categoría" )
+col2.bar_chart (dat.grafica_sub (ubicacion))
+col2.subheader ("Totales por subcategoria")
